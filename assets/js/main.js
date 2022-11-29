5ac78f8d74026cd37e35d3f0fae696530d41a60b
maiquocthinh <majquocthjnh@gmail.com>
2022-11-29 16:53:51 +0700

    Update some style

--- assets/js/main.js
+++ assets/js/main.js
@@ -94,3 +94,20 @@ if(btnShowHideMenu) {
         notActiveIcon.classList.add('hidden');
     }
 }
+
+// Scroll top
+$(document).ready(function () {
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 0) {
+            $('#scroll-top').fadeIn();
+        } else {
+            $('#scroll-top').fadeOut();
+        }
+    });
+    $('#scroll-top').click(function () {
+        $('body,html').animate({
+            scrollTop: 0
+        }, 400);
+        return false;
+    });
+});
